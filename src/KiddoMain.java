2972db2a457e7002ca467a7e5633adc343c9411c
Shreya Prakash <spraka52@asu.edu>
2025-04-15 21:58:09 -0700

    added changes to print the parse tree in a good format

--- src/KiddoMain.java
+++ src/KiddoMain.java
@@ -1,4 +1,5 @@
 import org.antlr.v4.runtime.*;
+import org.antlr.v4.runtime.tree.*;
 import antlr.KiddoLangLexer;
 import antlr.KiddoLangParser;
 import java.io.IOException;
@@ -16,10 +17,23 @@ public class KiddoMain {
             CommonTokenStream tokens = new CommonTokenStream(lexer);
             KiddoLangParser parser = new KiddoLangParser(tokens);
 
+            ParseTree tree = parser.program();
+
+            printPrettyTree(tree, parser, 0); 
 
         } catch (IOException e) {
             System.err.println("Error reading file: " + args[0]);
             e.printStackTrace();
+        }
+    }
+
+    public static void printPrettyTree(ParseTree tree, Parser parser, int indent) {
+        String indentStr = "  ".repeat(indent);
+        String nodeText = Trees.getNodeText(tree, parser);
+        System.out.println(indentStr + nodeText);
+
+        for (int i = 0; i < tree.getChildCount(); i++) {
+            printPrettyTree(tree.getChild(i), parser, indent + 1);
         }
     }
 }
